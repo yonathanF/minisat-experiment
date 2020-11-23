330771c5d4046908529590ad5e095327f353548b
SomeInterestingUserName <jchen2120212@gmail.com>
2020-11-22 19:14:02 -0500

    Add VSIDS options

--- minisat/core/run-test.sh
+++ minisat/core/run-test.sh
@@ -29,33 +29,36 @@ function run_with_options(){
     testname=$1
     options=$2
     testfile=$3
-    runsat=$( ./minisat -verb=1 $options $testfile )
+    extra_options="-cpu-lim=100"
+    runsat=$( ./minisat -verb=1 $options $extra_options $testfile )
     exitcode=$?
     echo "$( basename $testfile ),$testname,"$(parse_print_results "$runsat" $exitcode)
 }
 
 function run_tests(){
     testfile=$1
+    no_vsids="-no-DLIS -var-decay=0.999 -cla-decay=0.999"
+    vsids="-no-DLIS -var-decay=0.9 -cla-decay=0.9"
     # DPLL
-    run_with_options "DPLL" "-dis-learn -dis-restart -dis-2LW" $1
+    run_with_options "DPLL" "-dis-learn -dis-restart -dis-2LW $no_vsids" $1
     # CL
-    run_with_options "CL" "-no-dis-learn -dis-restart -dis-2LW" $1
+    run_with_options "CL" "-no-dis-learn -dis-restart -dis-2LW $no_vsids" $1
     # RST
-    run_with_options "RST" "-dis-learn -no-dis-restart -dis-2LW" $1
+    run_with_options "RST" "-dis-learn -no-dis-restart -dis-2LW $no_vsids" $1
     # 2WL
-    run_with_options "2WL" "-dis-learn -dis-restart -no-dis-2LW" $1
+    run_with_options "2WL" "-dis-learn -dis-restart -no-dis-2LW $no_vsids" $1
     # VSIDS
-
+    run_with_options "VSIDS" "-dis-learn -dis-restart -dis-2LW $vsids" $1
     # NOTCL
-
+    run_with_options "NOTCL" "-dis-learn -no-dis-restart -no-dis-2LW $vsids" $1
     # NOTRST
-
+    run_with_options "NOTRST" "-no-dis-learn -dis-restart -no-dis-2LW $vsids" $1
     # NOT2WL
-
+    run_with_options "NOT2WL" "-no-dis-learn -no-dis-restart -dis-2LW $vsids" $1
     # NOTVSIDS
-
+    run_with_options "NOTVSIDS" "-no-dis-learn -no-dis-restart -no-dis-2LW $no_vsids" $1
     # CDCL
-    run_with_options "CDCL" "" $1
+    run_with_options "CDCL" "-no-dis-learn -no-dis-restart -no-dis-2LW $vsids" $1
 }
 if [ "$#" -lt 1 ]; then
     echo "Usage: ./run-test.sh [cnf file(s)]"
